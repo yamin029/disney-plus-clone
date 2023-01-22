58c659c95859a398d9775de1d7e33be4ee859944
Yamin Hossain <yaminhossain@Yamins-MacBook-Air.local>
2023-01-22 07:52:40 +0600

    viewer added

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Viewers = () => {
-  return (
-    <div>Viewers</div>
-  )
+    return (
+        <Container>
+            <Wrap>
+                <img src="./images/viewers-disney.png" alt="disney" />
+            </Wrap>
+            <Wrap>
+                <img src="./images/viewers-pixar.png" alt="disney" />
+            </Wrap>
+            <Wrap>
+                <img src="./images/viewers-marvel.png" alt="disney" />
+            </Wrap>
+            <Wrap>
+                <img src="./images/viewers-starwars.png" alt="disney" />
+            </Wrap>
+            <Wrap>
+                <img src="./images/viewers-national.png" alt="disney" />
+            </Wrap>
+        </Container>
+    )
 }
 
 export default Viewers
+
+const Container = styled.div`
+    margin-top: 30px;
+    padding: 20px 0 30px;
+    display: grid;
+    grid-template-columns: repeat(5, minmax(0,1fr));
+    grid-gap: 25px;
+`
+const Wrap = styled.div`
+    border-radius: 10px;
+    border: 3px solid rgba(249,249,249,0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 /73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, .94) 0s;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    &:hover{
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 /72%) 0px 30px 20px -10px;
+        transform: scale(1.05);
+        cursor: pointer;
+    }
+`
